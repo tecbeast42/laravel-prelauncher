119fc7be1d75900d4c66c81b66d53be4abbcd42b
Tobias Kriebisch <t.kriebisch@gmail.com>
2015-05-18 23:34:43 +0200

    [Patch] Added a implicte Controller to Package for easier including in the route system

---
+++ src/Controllers/PreLaunchController.php
@@ -0,0 +1,26 @@
+<?php namespace TecBeast\PreLaunch\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PreLaunchController extends Controller {
+
+	public function getIndex() 
+	{
+		return "<h1>PreLaunch</h1>";
+	}
+
+	public function putIndex()
+	{
+
+	}
+
+	public function getPrivacy()
+	{
+
+	}
+
+	public function getImpressum()
+	{
+
+	}
+}
