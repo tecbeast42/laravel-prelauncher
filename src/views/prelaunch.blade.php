859550986a3269f84422bd0e1505de59cea60c27
Tobias Kriebisch <t.kriebisch@gmail.com>
2015-05-21 12:39:08 +0200

    [Patch] added error handling to view prelaunch

--- src/views/prelaunch.blade.php
+++ src/views/prelaunch.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 	<main>
-		<div class="msg-box">
-		</div>
+		@if(count($errors) > 0)
+			<div class="msg-box">
+				<ul>
+					@foreach($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
 		<div class="prelaunch-text">
 			<h3>Prelaunch</h3>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, ullam.
